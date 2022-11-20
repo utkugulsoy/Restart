ca8e1be81d1272d3fedc56aa347739b8bfa5fc73
Utku Kaan Gülsoy <47040490+utkugulsoy@users.noreply.github.com>
2022-11-21 00:53:53 +0300

    Initial commit.

---
+++ Restart/Screens/HomeView.swift
@@ -0,0 +1,71 @@
+//
+//  HomeView.swift
+//  Restart
+//
+//  Created by Utku Kaan Gülsoy on 20.11.2022.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @AppStorage("onboarding") var isOnboardingviewActive: Bool = false
+    @State private var isAnimating: Bool = false
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            //MARK: - HEADER
+            
+            Spacer()
+            ZStack {
+                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.2)
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                    .offset(y: isAnimating ? 35 : -35)
+                    .animation(
+                        Animation
+                            .easeInOut(duration: 4)
+                            .repeatForever()
+                        , value: isAnimating
+                    )
+                        
+            }
+            //MARK: - CENTER
+            Text("The time that leads to mastery is dependent on the intensity of our focus")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            //MARK: - FOOTER
+            Spacer()
+            Button(action:{
+                withAnimation {
+                    playSound(sound: "success", type: "m4a")
+                    isOnboardingviewActive = true
+                }
+            }){
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                Text("Restart")
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
+        }
+        .onAppear(perform: {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                isAnimating = true
+            })
+        })
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
