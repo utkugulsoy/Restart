8603651894526ed149f0060dd65e4b31793db13b
Utku Kaan Gülsoy <47040490+utkugulsoy@users.noreply.github.com>
2022-11-20 21:25:19 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Utku Kaan Gülsoy on 20.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
